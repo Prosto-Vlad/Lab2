da62d3ba233cc48fce255aabcfdd22b9cfe680f8
Prosto-Vlad <vladdeynega8@gmail.com>
2024-05-23 23:19:07 +0300

    Initial commit

---
+++ script1.cmd
@@ -0,0 +1,1 @@
+curl -L "https://kpi.eu.auth0.com/oauth/token" -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "grant_type=client_credentials&client_secret=ZRF8Op0tWM36p1_hxXTU-B0K_Gq_-eAVtlrQpY24CasYiDmcXBhNS6IJMNcz1EgB&client_id=JIvCO5c2IBHlAe2patn6l6q5H35qxti0&audience=https://kpi.eu.auth0.com/api/v2/"
